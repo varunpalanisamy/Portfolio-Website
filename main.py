b801efcdd259acb1f1585dbb6828092ff73a0ef2
Varun <varun.palanisamy@gmail.com>
2024-07-01 15:41:42 -0700

    Added description, image, and links

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 
@@ -18,3 +19,31 @@ My goal is to contribute to innovative tech solutions and to continuously grow a
     
     """
     st.info(content)
+
+content2 = """
+
+You can find some of the apps I have built. Feel free to contact me!
+
+"""
+st.write(content2)
+
+
+col3,empty_col, col4 = st.columns([1.5,0.5,1.5])
+
+
+df = pandas.read_csv("data (1).csv", sep = ";")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images (2)/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
+
+
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images (2)/" + row["image"])
